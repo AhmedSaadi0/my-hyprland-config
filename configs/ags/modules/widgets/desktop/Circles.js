1fdecc683471f421d0f881a8e06149ecc35fb5db
ahmedsaadi0 <cr4ins@gmail.com>
2023-12-31 03:12:35 +0300

    remember last selected theme & change animations

--- configs/ags/modules/widgets/desktop/Circles.js
+++ configs/ags/modules/widgets/desktop/Circles.js
@@ -4,6 +4,24 @@ import { Mpris, Widget } from "../../utils/imports.js";
 import { selectedMusicPlayer } from "../MusicPLayer.js";
 
 
+// const progress = Widget.CircularProgress({
+//     style:
+//         'min-width: 50px;' + // its size is min(min-height, min-width)
+//         'min-height: 50px;' +
+//         'font-size: 6px;' + // to set its thickness set font-size on it
+//         'margin: 4px;' + // you can set margin on it
+//         'background-color: #131313;' + // set its bg color
+//         'color: aqua;', // set its fg color
+
+//     value: Battery.bind('percent').transform(p => p / 100),
+//     child: Widget.Icon({
+//         icon: Battery.bind('icon-name'),
+//     }),
+//     rounded: false,
+//     inverted: false,
+//     startAt: 0.75,
+// });
+
 const MusicWidget = Widget.Box({
     spacing: 18,
     homogeneous: false,
