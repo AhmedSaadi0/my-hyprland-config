56e067db885f855afd665f27689ff6cb22e74eff
ahmedsaadi0 <cr4ins@gmail.com>
2024-03-17 18:25:26 +0300

    Add new sounds effect

--- config/ags/config.js
+++ config/ags/config.js
@@ -1,25 +1,24 @@
-import App from "resource:///com/github/Aylur/ags/app.js";
-import Notifications from 'resource:///com/github/Aylur/ags/service/notifications.js';
-import { Bar } from "./modules/topbar.js";
-import { LeftMenu } from "./modules/menus/left_menu.js";
-import { HardwareMenu } from "./modules/menus/HardwareMenu.js";
-import { NotificationCenter } from "./modules/menus/notification_center.js";
-import { VolumeOSD } from "./modules/on-screen/volume.js";
+import { Utils, App, Notifications } from './modules/utils/imports.js';
+import { Bar } from './modules/topbar.js';
+import { LeftMenu } from './modules/menus/left_menu.js';
+import { HardwareMenu } from './modules/menus/HardwareMenu.js';
+import { NotificationCenter } from './modules/menus/notification_center.js';
+import { VolumeOSD } from './modules/on-screen/volume.js';
 import MyNotifications from './modules/notifications/OSDNotifications.js';
 import { PrayerTimesMenu } from './modules/menus/PrayerTimesMenu.js';
 import ColorWidget from './modules/widgets/desktop/ColorsWidget.js';
-import win20Widget from "./modules/widgets/desktop/Win20Widget.js";
-import materialWidget from "./modules/widgets/desktop/MaterialYouOne.js";
-import unicatWidget from "./modules/widgets/desktop/UnicatWidget.js";
-import blackHoleWidget from "./modules/widgets/desktop/BlackHole.js";
-import goldenWidget from "./modules/widgets/desktop/Golden.js";
+import win20Widget from './modules/widgets/desktop/Win20Widget.js';
+import materialWidget from './modules/widgets/desktop/MaterialYouOne.js';
+import unicatWidget from './modules/widgets/desktop/UnicatWidget.js';
+import blackHoleWidget from './modules/widgets/desktop/BlackHole.js';
+import goldenWidget from './modules/widgets/desktop/Golden.js';
 import harmonyWidget from './modules/widgets/desktop/Harmony.js';
 import newCatWidget from './modules/widgets/desktop/NewCat.js';
 import deerWidget from './modules/widgets/desktop/DeerWidget.js';
 import circlesMusicWidget from './modules/widgets/desktop/Circles.js';
 import whiteFlowerWidget from './modules/widgets/desktop/WhiteFlower.js';
-import { Utils } from './modules/utils/imports.js';
-import { WeatherMenu } from "./modules/menus/WeatherMenu.js";
+import { WeatherMenu } from './modules/menus/WeatherMenu.js';
+import settings from './modules/settings.js';
 // import ScreenCorners from './modules/components/ScreenCorners.js';
 
 // in config.js
@@ -29,44 +28,46 @@ const css = App.configDir + '/style.css';
 Utils.exec(`sassc ${scss} ${css}`);
 
 let windows = [
-    // Bar({monitor : 1}),
-    // Bar({ monitor: 0 }),
-    VolumeOSD(),
-    MyNotifications(),
-    NotificationCenter(),
-    HardwareMenu(),
-    PrayerTimesMenu(),
-    WeatherMenu(),
+  // Bar({monitor : 1}),
+  // Bar({ monitor: 0 }),
+  VolumeOSD(),
+  MyNotifications(),
+  NotificationCenter(),
+  HardwareMenu(),
+  PrayerTimesMenu(),
+  WeatherMenu(),
 
-    // ... Desktop widgets ... //
-    ColorWidget,
-    win20Widget,
-    materialWidget,
-    unicatWidget,
-    blackHoleWidget,
-    goldenWidget,
-    harmonyWidget,
-    newCatWidget,
-    deerWidget,
-    circlesMusicWidget,
-    whiteFlowerWidget,
+  // ... Desktop widgets ... //
+  ColorWidget,
+  win20Widget,
+  materialWidget,
+  unicatWidget,
+  blackHoleWidget,
+  goldenWidget,
+  harmonyWidget,
+  newCatWidget,
+  deerWidget,
+  circlesMusicWidget,
+  whiteFlowerWidget,
 ];
 
-const screens = JSON.parse(Utils.exec("hyprctl monitors all -j"));
+const screens = JSON.parse(Utils.exec('hyprctl monitors all -j'));
 
 for (let i = 0; i < screens.length; i++) {
-    const screen = screens[i];
+  const screen = screens[i];
 
-    windows.push(Bar({ monitor: screen.id }));
+  windows.push(Bar({ monitor: screen.id }));
 
-    const leftMene = LeftMenu({ monitor: screen.id });
-    windows.push(leftMene);
+  const leftMene = LeftMenu({ monitor: screen.id });
+  windows.push(leftMene);
 }
 
 export default {
-    css: css,
-    cacheNotificationActions: true,
-    windows: windows,
+  css: css,
+  // cacheNotificationActions: true,
+  windows: windows,
 };
-
+Notifications.cacheActions;
 globalThis.getNot = () => Notifications;
+
+Utils.execAsync([`paplay`, settings.assets.audio.desktop_login]).catch(print);
--- config/ags/modules/settings.js
+++ config/ags/modules/settings.js
@@ -4,54 +4,56 @@ const MAIN_PATH = `${App.configDir}`;
 const ASSETS_PATH = `${App.configDir}/assets`;
 
 const getAssets = (path) => {
-    return `${ASSETS_PATH}/${path}`;
+  return `${ASSETS_PATH}/${path}`;
 };
 
 const getPath = (path) => {
-    return `${MAIN_PATH}/${path}`;
+  return `${MAIN_PATH}/${path}`;
 };
 
 const settings = {
-    assets: {
-        wallpapers: getAssets('wallpapers'),
-        icons: {
-            hot_weather: `${getAssets('icons')}/hot-weather.png`,
-            cold_weather: `${getAssets('icons')}/cold-weather.png`,
-            mosque: `${getAssets('icons')}/mosque.png`,
-        },
-        audio: {
-            cold_weather: `${getAssets('audio')}/cold-weather.mp3`,
-            prayer_time: `${getAssets('audio')}/prayer-notification.ogg`,
-        },
+  assets: {
+    wallpapers: getAssets('wallpapers'),
+    icons: {
+      hot_weather: `${getAssets('icons')}/hot-weather.png`,
+      cold_weather: `${getAssets('icons')}/cold-weather.png`,
+      mosque: `${getAssets('icons')}/mosque.png`,
     },
-    scripts: {
-        scripts: getPath('scripts'),
-        dynamicM3Py: getPath('scripts/m3/dynamic-m3.py'),
-        get_wallpapers: getPath('scripts/get_wallpapers.sh'),
+    audio: {
+      cold_weather: `${getAssets('audio')}/cold-weather.mp3`,
+      prayer_time: `${getAssets('audio')}/prayer-notification.ogg`,
+      desktop_login: `${getAssets('audio')}/desktop-login.mp3`,
+      desktop_logout: `${getAssets('audio')}/desktop-logout.mp3`,
     },
-    theme: {
-        scss: `${getPath('scss')}`,
-        absoluteDynamicM3Scss: `${getPath('scss/themes/m3/dynamic.scss')}`,
-        mainCss: `${getPath('/scss/main.scss')}`,
-        styleCss: `${getPath('/style.scss')}`,
+  },
+  scripts: {
+    scripts: getPath('scripts'),
+    dynamicM3Py: getPath('scripts/m3/dynamic-m3.py'),
+    get_wallpapers: getPath('scripts/get_wallpapers.sh'),
+  },
+  theme: {
+    scss: `${getPath('scss')}`,
+    absoluteDynamicM3Scss: `${getPath('scss/themes/m3/dynamic.scss')}`,
+    mainCss: `${getPath('/scss/main.scss')}`,
+    styleCss: `${getPath('/style.scss')}`,
+  },
+  weather: {
+    // provider is 'ar.wttr.in'
+    language: 'ar', // Not implemented yot - only arabic is supported
+    location: 'sanaa',
+    format: 'j1',
+  },
+  prayerTimes: {
+    // provider is 'api.aladhan.com'
+    city: 'sanaa',
+    country: 'yemen',
+  },
+  hardware: {
+    network: {
+      rx_path: '/sys/class/net/wlp0s20f3/statistics/tx_bytes',
+      tx_path: '/sys/class/net/wlp0s20f3/statistics/tx_bytes',
     },
-    weather: {
-        // provider is 'ar.wttr.in'
-        language: 'ar', // Not implemented yot - only arabic is supported
-        location: 'sanaa',
-        format: 'j1',
-    },
-    prayerTimes: {
-        // provider is 'api.aladhan.com'
-        city: 'sanaa',
-        country: 'yemen',
-    },
-    hardware: {
-        network: {
-            rx_path: '/sys/class/net/wlp0s20f3/statistics/tx_bytes',
-            tx_path: '/sys/class/net/wlp0s20f3/statistics/tx_bytes',
-        },
-    },
+  },
 };
 
 export default settings;
