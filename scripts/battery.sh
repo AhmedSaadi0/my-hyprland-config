249dfa1d7ed81f1828c4b7b6ec74a2bec450cf5c
ahmedsaadi0 <ahmed0yemen@gmail.com>
2024-07-13 11:15:37 +0300

    imorove scripts

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -16,25 +16,25 @@ high_battery_sound="$home_path//.config/ags/assets/audio/battery-high.mp3"
 
 # Set XDG_RUNTIME_DIR if not already set
 if [ -z "$XDG_RUNTIME_DIR" ]; then
-    export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+	export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 fi
 
 # Set DBUS_SESSION_BUS_ADDRESS if not already set
 if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-    export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
+	export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
 fi
 
 # Set icon and message based on battery level
 if [[ $charging_status == "Discharging" && $battery_percentage -lt 40 ]]; then
-    icon_name="battery-empty"
-    message="نسبة البطارية أقل من 40%. حسب قاعدة 40-80 يفضل شحن جهازك المحمول الان."
+	icon_name="battery-empty"
+	message="نسبة البطارية أقل من 40%. حسب قاعدة 40-80 يفضل شحن جهازك المحمول الان."
 
-    notify-send -i "$icon_name" -t "$show_time" "نسبة شحن البطارية ("$battery_percentage"%)" "$message"
-    paplay $low_battery_sound
+	notify-send -i "$icon_name" -t "$show_time" "نسبة شحن البطارية ("$battery_percentage"%)" "$message"
+	paplay $low_battery_sound
 elif [[ ($charging_status == "Charging" || $charging_status == "Not charging") && $battery_percentage -gt 80 ]]; then
-    icon_name="battery-full"
-    message="نسبة البطارية أعلى من 80%. حسب قاعدة 40-80 يفضل فصل الشاحن الان"
+	icon_name="battery-full"
+	message="نسبة البطارية أعلى من 80%. حسب قاعدة 40-80 يفضل فصل الشاحن الان"
 
-    notify-send -i "$icon_name" -t "$show_time" "نسبة شحن البطارية ("$battery_percentage"%)" "$message"
-    paplay $high_battery_sound
+	notify-send -i "$icon_name" -t "$show_time" "نسبة شحن البطارية ("$battery_percentage"%)" "$message"
+	paplay $high_battery_sound
 fi
