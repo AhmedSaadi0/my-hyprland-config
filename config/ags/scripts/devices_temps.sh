9bfaf08b46523655d207e8df470c20ff7cea1673
abdullahalhakimi <abdullahalhakimi7@gmail.com>
2024-07-24 22:03:28 +0300

    edit devices temps script

--- config/ags/scripts/devices_temps.sh
+++ config/ags/scripts/devices_temps.sh
@@ -3,27 +3,44 @@
 sensor_data=$(sensors -u)
 
 get_sensor_temp() {
-    label=$1
-    temp=$(echo "$sensor_data" | awk -v label="$label" '
-        /^'"$label"'/ {
-            if (!/pch_cannonlake-virtual-0/) {
-                getline; sub(/\+/, "", $2); print $2; exit
-            }
+    section=$1
+    label=$2
+    temp=$(echo "$sensor_data" | awk -v section="$section" -v label="$label" '
+        BEGIN { in_section=0 }
+        $0 ~ section {
+            in_section=1
+        }
+        in_section && $1 ~ label {
+            print $2
+            exit
+        }
+        /^[[:space:]]*$/ {
+            in_section=0
         }
     ')
-    echo "$temp"
+    if [ -z "$temp" ]; then
+        echo "null"
+    else
+        echo "$temp"
+    fi
 }
 
+
+echo "iwlwifi_1-virtual-0 section:" >&2
+echo "$sensor_data" | awk '/iwlwifi_1-virtual-0/,/^[[:space:]]*$/' >&2
+echo "nvme-pci-0300 section:" >&2
+echo "$sensor_data" | awk '/nvme-pci-0300/,/^[[:space:]]*$/' >&2
+echo "coretemp-isa-0000 section:" >&2
+echo "$sensor_data" | awk '/coretemp-isa-0000/,/^[[:space:]]*$/' >&2
+
+
 echo "{
-  \"wifi\": $(get_sensor_temp 'temp1'),
-  \"nvme_total\": $(get_sensor_temp 'Composite'),
-  \"nvme_sensor1\": $(get_sensor_temp 'Sensor 1:'),
-  \"nvme_sensor2\": $(get_sensor_temp 'Sensor 2:'),
-  \"cpu_total\": $(get_sensor_temp 'Package id 0:'),
-  \"cpu_core0\": $(get_sensor_temp 'Core 0:'),
-  \"cpu_core1\": $(get_sensor_temp 'Core 1:'),
-  \"cpu_core2\": $(get_sensor_temp 'Core 2:'),
-  \"cpu_core3\": $(get_sensor_temp 'Core 3:'),
-  \"cpu_core4\": $(get_sensor_temp 'Core 4:'),
-  \"cpu_core5\": $(get_sensor_temp 'Core 5:')
+  \"wifi\": $(get_sensor_temp 'iwlwifi_1-virtual-0' 'temp1_input:'),
+  \"nvme_total\": $(get_sensor_temp 'nvme-pci-0300' 'temp1_input:'),
+  \"nvme_sensor1\": $(get_sensor_temp 'nvme-pci-0300' 'temp2_input:'),
+  \"cpu_total\": $(get_sensor_temp 'coretemp-isa-0000' 'temp1_input:'),
+  \"cpu_core0\": $(get_sensor_temp 'coretemp-isa-0000' 'temp2_input:'),
+  \"cpu_core1\": $(get_sensor_temp 'coretemp-isa-0000' 'temp3_input:'),
+  \"cpu_core2\": $(get_sensor_temp 'coretemp-isa-0000' 'temp4_input:'),
+  \"cpu_core3\": $(get_sensor_temp 'coretemp-isa-0000' 'temp5_input:')
 }"
