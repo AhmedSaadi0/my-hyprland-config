f33d9f5935f92428af0edde943146435b7f5c0d7
AhmedSaadi0 <cr9ine@gmail.com>
2026-04-24 04:37:24 +0300

    Fix Void dependencies - pull #73 in new installer

--- nibras_installer/modules/dependencies.py
+++ nibras_installer/modules/dependencies.py
@@ -87,7 +87,7 @@ def install_void(install_optional):
     )
     run_command_verbose("sudo xbps-install -S")
 
-    required_pkgs = "hyprland quickshell plasma-nm playerctl polkit-kde-agent dolphin konsole brightnessctl gammastep wl-clipboard sysstat bc sassc systemsettings acpi fish-shell gnome-bluetooth power-profiles-daemon lm_sensors CopyQ vnstat nethogs xz swww jq python3 python3-devel python313"
+    required_pkgs = "hyprland quickshell plasma-nm playerctl polkit-kde-agent dolphin konsole brightnessctl gammastep wl-clipboard sysstat bc sassc systemsettings acpi fish-shell gnome-bluetooth power-profiles-daemon lm_sensors CopyQ vnstat nethogs xz swww jq python3-devel dbus-devel glib-devel cmake"
     optional_pkgs = (
         "strawberry easyeffects blueman telegram-desktop kvantum firefox"
     )
